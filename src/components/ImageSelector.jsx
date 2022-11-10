f68c6f15873bbb5b2f88c4e6dfcd543a8cc64de7
pablormacia <pablo-macia@live.com.ar>
2022-11-09 22:46:30 -0300

    Working in Image Profile

---
+++ src/components/ImageSelector.jsx
@@ -0,0 +1,70 @@
+import  React, {useState} from 'react' ;
+import { View, Text, StyleSheet,Button, Image, Alert } from 'react-native' ;
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+const ImageSelector = ({onImage}) => {
+    const [pickedUri,setPickedUri] = useState();
+
+    const verifyPermissions = async () => {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    
+        if (status !== "granted") {
+          Alert.alert("Permiso denegado", "Necesitamos permisos para usar la cámara", [{ text: "Ok" }]);
+          return false;
+        }
+        return true;
+      };
+
+      const handlerTakeImage = async () => {
+        const isCameraOk = await verifyPermissions();
+        if (!isCameraOk) return;
+    
+        const image = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [16, 9],
+            quality: 0.7,
+        });
+    
+        setPickedUri(image.uri);
+        onImage(image.uri);
+      };
+
+    return(
+        <View style={styles.container}>
+                <View style={styles.preview}>
+                    {!pickedUri ? (
+                        <Image style={styles.image} source={{uri: 'https://mspgh.unimelb.edu.au/__data/assets/image/0011/3576098/Placeholder.jpg'}}/>
+                    ) : (
+                        <Image resizeMode={'cover'} style={styles.image} source={{uri: pickedUri}}  />
+                    )}
+                </View>
+                <Button
+                    title="Tomar foto"
+                    onPress={handlerTakeImage}
+                />
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10,
+    },
+    preview: {
+        width:200,
+        height: 200,
+        borderRadius: '50%',
+        margin: 10,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    }
+})
+
+export default ImageSelector;
--- src/screens/User.jsx
+++ src/screens/User.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Text } from "react-native";
+import React, {useState} from "react";
+import { Button, Text, View } from "react-native";
+import ImageSelector from "../components/ImageSelector";
 
 
 const User = () => {
+    const [image, setImage] = useState("");
+
+
+    const onHandleImageSelect = (imageUri) => {
+        setImage(imageUri);
+      };
+
     return(
-        <Text>Perfil de usuario</Text>
+        <View>
+            <Text>Perfil de usuario</Text>
+            <ImageSelector onImage={onHandleImageSelect} />
+        </View>
     )
 }
 
